20ca5d6cd96806b05c4c1d89f67ccb05b1e3cbd2
o.zambotti <o.zambotti@trentorise.eu>
2014-02-27 09:56:50 +0100

    First changes in the fork

--- ExtendedCalendarView/src/com/tyczj/extendedcalendarview/CalendarAdapter.java
+++ ExtendedCalendarView/src/com/tyczj/extendedcalendarview/CalendarAdapter.java
@@ -8,28 +8,30 @@ import java.util.concurrent.TimeUnit;
 
 import android.content.Context;
 import android.text.format.Time;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class CalendarAdapter extends BaseAdapter{
-	
-	static final int FIRST_DAY_OF_WEEK =0;
+public class CalendarAdapter extends BaseAdapter {
+
 	Context context;
 	Calendar cal;
+	int firstDayOfWeek = Calendar.SUNDAY - 1; // 0
 	public String[] days;
-//	OnAddNewEventClick mAddEvent;
-	
+	// OnAddNewEventClick mAddEvent;
+
 	ArrayList<Day> dayList = new ArrayList<Day>();
-	
-	public CalendarAdapter(Context context, Calendar cal){
+
+	public CalendarAdapter(Context context, Calendar cal, int firstDayOfWeek) {
 		this.cal = cal;
 		this.context = context;
 		cal.set(Calendar.DAY_OF_MONTH, 1);
+		this.firstDayOfWeek = firstDayOfWeek - 1;
 		refreshDays();
 	}
 
@@ -47,78 +49,98 @@ public class CalendarAdapter extends BaseAdapter{
 	public long getItemId(int position) {
 		return 0;
 	}
-	
-	public int getPrevMonth(){
-		if(cal.get(Calendar.MONTH) == cal.getActualMinimum(Calendar.MONTH)){
-			cal.set(Calendar.YEAR, cal.get(Calendar.YEAR-1));
-		}else{
-			
+
+	public int getPrevMonth() {
+		if (cal.get(Calendar.MONTH) == cal.getActualMinimum(Calendar.MONTH)) {
+			cal.set(Calendar.YEAR, cal.get(Calendar.YEAR - 1));
+		} else {
+
 		}
 		int month = cal.get(Calendar.MONTH);
-		if(month == 0){
+		if (month == 0) {
 			return month = 11;
 		}
-		
-		return month-1;
+
+		return month - 1;
 	}
-	
-	public int getMonth(){
+
+	public int getMonth() {
 		return cal.get(Calendar.MONTH);
 	}
 
 	@Override
 	public View getView(final int position, View convertView, ViewGroup parent) {
 		View v = convertView;
-		LayoutInflater vi = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		if(position >= 0 && position < 7){
+		LayoutInflater vi = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		if (position >= 0 && position < 7) {
 			v = vi.inflate(R.layout.day_of_week, null);
-			TextView day = (TextView)v.findViewById(R.id.textView1);
-			
-			if(position == 0){
-				day.setText(R.string.sunday);
-			}else if(position == 1){
-				day.setText(R.string.monday);
-			}else if(position == 2){
-				day.setText(R.string.tuesday);
-			}else if(position == 3){
-				day.setText(R.string.wednesday);
-			}else if(position == 4){
-				day.setText(R.string.thursday);
-			}else if(position == 5){
-				day.setText(R.string.friday);
-			}else if(position == 6){
-				day.setText(R.string.saturday);
+			TextView day = (TextView) v.findViewById(R.id.day_of_week_textView);
+			day.setGravity(Gravity.CENTER);
+
+			switch (firstDayOfWeek) {
+			case 1:
+				if (position == 0) {
+					day.setText(R.string.monday);
+				} else if (position == 1) {
+					day.setText(R.string.tuesday);
+				} else if (position == 2) {
+					day.setText(R.string.wednesday);
+				} else if (position == 3) {
+					day.setText(R.string.thursday);
+				} else if (position == 4) {
+					day.setText(R.string.friday);
+				} else if (position == 5) {
+					day.setText(R.string.saturday);
+				} else if (position == 6) {
+					day.setText(R.string.sunday);
+				}
+				break;
+			default:
+				if (position == 0) {
+					day.setText(R.string.sunday);
+				} else if (position == 1) {
+					day.setText(R.string.monday);
+				} else if (position == 2) {
+					day.setText(R.string.tuesday);
+				} else if (position == 3) {
+					day.setText(R.string.wednesday);
+				} else if (position == 4) {
+					day.setText(R.string.thursday);
+				} else if (position == 5) {
+					day.setText(R.string.friday);
+				} else if (position == 6) {
+					day.setText(R.string.saturday);
+				}
+				break;
 			}
-			
-		}else{
-			
-	        v = vi.inflate(R.layout.day_view, null);
-			
-			TextView dayTV = (TextView)v.findViewById(R.id.textView1);
-			RelativeLayout rl = (RelativeLayout)v.findViewById(R.id.rl);
-			ImageView iv = (ImageView)v.findViewById(R.id.imageView1);
-			ImageView blue = (ImageView)v.findViewById(R.id.imageView2);
-			ImageView purple = (ImageView)v.findViewById(R.id.imageView3);
-			ImageView green = (ImageView)v.findViewById(R.id.imageView4);
-			ImageView orange = (ImageView)v.findViewById(R.id.imageView5);
-			ImageView red = (ImageView)v.findViewById(R.id.imageView6);	
-			
+		} else {
+			v = vi.inflate(R.layout.day_view, null);
+
+			TextView dayTV = (TextView) v.findViewById(R.id.textView1);
+			LinearLayout rl = (LinearLayout) v.findViewById(R.id.rl);
+			ImageView iv = (ImageView) v.findViewById(R.id.imageView1);
+			ImageView blue = (ImageView) v.findViewById(R.id.imageView2);
+			ImageView purple = (ImageView) v.findViewById(R.id.imageView3);
+			ImageView green = (ImageView) v.findViewById(R.id.imageView4);
+			ImageView orange = (ImageView) v.findViewById(R.id.imageView5);
+			ImageView red = (ImageView) v.findViewById(R.id.imageView6);
+
 			blue.setVisibility(View.VISIBLE);
 			purple.setVisibility(View.VISIBLE);
 			green.setVisibility(View.VISIBLE);
 			purple.setVisibility(View.VISIBLE);
 			orange.setVisibility(View.VISIBLE);
 			red.setVisibility(View.VISIBLE);
-			
+
 			iv.setVisibility(View.VISIBLE);
 			dayTV.setVisibility(View.VISIBLE);
 			rl.setVisibility(View.VISIBLE);
-			
+
 			Day day = dayList.get(position);
-			
-			if(day.getNumOfEvenets() > 0){
+
+			if (day.getNumOfEvenets() > 0) {
 				Set<Integer> colors = day.getColors();
-				
+
 				iv.setVisibility(View.INVISIBLE);
 				blue.setVisibility(View.INVISIBLE);
 				purple.setVisibility(View.INVISIBLE);
@@ -126,27 +148,27 @@ public class CalendarAdapter extends BaseAdapter{
 				purple.setVisibility(View.INVISIBLE);
 				orange.setVisibility(View.INVISIBLE);
 				red.setVisibility(View.INVISIBLE);
-				
-				if(colors.contains(0)){
+
+				if (colors.contains(0)) {
 					iv.setVisibility(View.VISIBLE);
 				}
-				if(colors.contains(2)){
+				if (colors.contains(2)) {
 					blue.setVisibility(View.VISIBLE);
 				}
-				if(colors.contains(4)){
+				if (colors.contains(4)) {
 					purple.setVisibility(View.VISIBLE);
 				}
-				if(colors.contains(5)){
+				if (colors.contains(5)) {
 					green.setVisibility(View.VISIBLE);
 				}
-				if(colors.contains(3)){
+				if (colors.contains(3)) {
 					orange.setVisibility(View.VISIBLE);
 				}
-				if(colors.contains(1)){
+				if (colors.contains(1)) {
 					red.setVisibility(View.VISIBLE);
 				}
-				
-			}else{
+
+			} else {
 				iv.setVisibility(View.INVISIBLE);
 				blue.setVisibility(View.INVISIBLE);
 				purple.setVisibility(View.INVISIBLE);
@@ -155,81 +177,79 @@ public class CalendarAdapter extends BaseAdapter{
 				orange.setVisibility(View.INVISIBLE);
 				red.setVisibility(View.INVISIBLE);
 			}
-				
-			if(day.getDay() == 0){
+
+			if (day.getDay() == 0) {
 				rl.setVisibility(View.GONE);
-			}else{
+			} else {
 				dayTV.setVisibility(View.VISIBLE);
 				dayTV.setText(String.valueOf(day.getDay()));
 			}
 		}
-		
+
 		return v;
 	}
-	
-	public void refreshDays()
-    {
-    	// clear items
-    	dayList.clear();
-    	
-    	int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH)+7;
-        int firstDay = (int)cal.get(Calendar.DAY_OF_WEEK);
-        int year = cal.get(Calendar.YEAR);
-        int month = cal.get(Calendar.MONTH);
-        TimeZone tz = TimeZone.getDefault();
-        
-        // figure size of the array
-        if(firstDay==1){
-        	days = new String[lastDay+(FIRST_DAY_OF_WEEK*6)];
-        }
-        else {
-        	days = new String[lastDay+firstDay-(FIRST_DAY_OF_WEEK+1)];
-        }
-        
-        int j=FIRST_DAY_OF_WEEK;
-        
-        // populate empty days before first real day
-        if(firstDay>1) {
-	        for(j=0;j<(firstDay-FIRST_DAY_OF_WEEK)+7;j++) {
-	        	days[j] = "";
-	        	Day d = new Day(context,0,0,0);
-	        	dayList.add(d);
-	        }
-        }
-	    else {
-	    	for(j=0;j<(FIRST_DAY_OF_WEEK*6)+7;j++) {
-	        	days[j] = "";
-	        	Day d = new Day(context,0,0,0);
-	        	dayList.add(d);
-	        }
-	    	j=FIRST_DAY_OF_WEEK*6+1; // sunday => 1, monday => 7
-	    }
-        
-        // populate days
-        int dayNumber = 1;
-        
-        if(j>0 && dayList.size() > 0 && j != 1){
-        	dayList.remove(j-1);
-        }
-        
-        for(int i=j-1;i<days.length;i++) {
-        	Day d = new Day(context,dayNumber,year,month);
-        	
-        	Calendar cTemp = Calendar.getInstance();
-        	cTemp.set(year, month, dayNumber);
-        	int startDay = Time.getJulianDay(cTemp.getTimeInMillis(), TimeUnit.MILLISECONDS.toSeconds(tz.getOffset(cTemp.getTimeInMillis())));
-        	
-        	d.setAdapter(this);
-        	d.setStartDay(startDay);
-        	
-        	days[i] = ""+dayNumber;
-        	dayNumber++;
-        	dayList.add(d);
-        }
-    }
-	
-//	public abstract static class OnAddNewEventClick{
-//		public abstract void onAddNewEventClick();
-//	}
-	
+
+	public void refreshDays() {
+		// clear items
+		dayList.clear();
+
+		int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH) + 7;
+		int firstDay = (int) cal.get(Calendar.DAY_OF_WEEK);
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		TimeZone tz = TimeZone.getDefault();
+
+		// figure size of the array
+		if (firstDay == 1) {
+			days = new String[lastDay + (firstDayOfWeek * 6)];
+		} else {
+			days = new String[lastDay + firstDay - (firstDayOfWeek + 1)];
+		}
+
+		int j = firstDayOfWeek;
+
+		// populate empty days before first real day
+		if (firstDay > 1) {
+			for (j = 0; j < (firstDay - firstDayOfWeek) + 7; j++) {
+				days[j] = "";
+				Day d = new Day(context, 0, 0, 0);
+				dayList.add(d);
+			}
+		} else {
+			for (j = 0; j < (firstDayOfWeek * 6) + 7; j++) {
+				days[j] = "";
+				Day d = new Day(context, 0, 0, 0);
+				dayList.add(d);
+			}
+			j = firstDayOfWeek * 6 + 1; // sunday => 1, monday => 7
+		}
+
+		// populate days
+		int dayNumber = 1;
+
+		if (j > 0 && dayList.size() > 0 && j != 1) {
+			dayList.remove(j - 1);
+		}
+
+		for (int i = j - 1; i < days.length; i++) {
+			Day d = new Day(context, dayNumber, year, month);
+
+			Calendar cTemp = Calendar.getInstance();
+			cTemp.set(year, month, dayNumber);
+			int startDay = Time.getJulianDay(cTemp.getTimeInMillis(),
+					TimeUnit.MILLISECONDS.toSeconds(tz.getOffset(cTemp.getTimeInMillis())));
+
+			d.setAdapter(this);
+			d.setStartDay(startDay);
+
+			days[i] = "" + dayNumber;
+			dayNumber++;
+			dayList.add(d);
+		}
+	}
+
+	// public abstract static class OnAddNewEventClick{
+	// public abstract void onAddNewEventClick();
+	// }
+
 }
--- ExtendedCalendarView/src/com/tyczj/extendedcalendarview/ExtendedCalendarView.java
+++ ExtendedCalendarView/src/com/tyczj/extendedcalendarview/ExtendedCalendarView.java
@@ -11,35 +11,40 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnCreateContextMenuListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.FrameLayout;
 import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.view.View.OnCreateContextMenuListener;
-import android.view.View.OnClickListener;
 
-public class ExtendedCalendarView extends RelativeLayout implements OnItemClickListener,OnCreateContextMenuListener,
-	OnClickListener{
-	
+public class ExtendedCalendarView extends FrameLayout implements OnItemClickListener, OnCreateContextMenuListener,
+		OnClickListener {
+
 	private Context context;
 	private OnDayClickListener dayListener;
 	private OnDayContextMenuListener contextListener;
-	private GridView calendar;
-	private CalendarAdapter mAdapter;
+
+	private LinearLayout calendarLayout;
+	private LinearLayout calendarMonthLayout;
+	private ImageButton next, prev;
+	private TextView monthTextView;
+	private GridView calendarGridView;
+
 	private Calendar cal;
-	private TextView month;
-	private RelativeLayout base;
-	private ImageView next,prev;
-	
-	public interface OnDayClickListener{
+	private CalendarAdapter mAdapter;
+
+	public interface OnDayClickListener {
 		public void onDayClicked(AdapterView<?> adapter, View view, int position, long id, Day day);
 	}
-	
-	public interface OnDayContextMenuListener{
-		public void onDayContextMenu(ContextMenu menu, View v,ContextMenuInfo menuInfo);
+
+	public interface OnDayContextMenuListener {
+		public void onDayContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo);
 	}
 
 	public ExtendedCalendarView(Context context) {
@@ -47,260 +52,191 @@ public class ExtendedCalendarView extends RelativeLayout implements OnItemClickL
 		this.context = context;
 		init();
 	}
-	
+
 	public ExtendedCalendarView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context = context;
 		init();
 	}
-	
-	public ExtendedCalendarView(Context context, AttributeSet attrs,int defStyle) {
+
+	public ExtendedCalendarView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		this.context = context;
 		init();
 	}
-	
-	private void init(){
-		cal = Calendar.getInstance();
-		base = new RelativeLayout(context);
-		base.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.WRAP_CONTENT));
-		base.setMinimumHeight(50);
-		
-		base.setId(4);
-		
-		LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-		params.leftMargin = 16;
-		params.topMargin = 50;
-		params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-		params.addRule(RelativeLayout.CENTER_VERTICAL);
-		prev = new ImageView(context);
-		prev.setId(1);
-		prev.setLayoutParams(params);
-		prev.setImageResource(R.drawable.navigation_previous_item);
+
+	private void init() {
+		cal = Calendar.getInstance(Locale.getDefault());
+		LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		calendarLayout = (LinearLayout) layoutInflater.inflate(R.layout.calendar_layout, null);
+		calendarMonthLayout = (LinearLayout) calendarLayout.findViewById(R.id.calendar_month_layout);
+
+		prev = (ImageButton) calendarMonthLayout.findViewById(R.id.month_prev_btn);
 		prev.setOnClickListener(this);
-		base.addView(prev);
-		
-		params = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-		params.addRule(RelativeLayout.CENTER_HORIZONTAL);
-		params.addRule(RelativeLayout.CENTER_VERTICAL);
-		month = new TextView(context);
-		month.setId(2);
-		month.setLayoutParams(params);
-		month.setTextAppearance(context, android.R.attr.textAppearanceLarge);
-		month.setText(cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.US));
-		month.setTextSize(30);
-		
-		base.addView(month);
-		
-		params = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-		params.rightMargin = 16;
-		params.topMargin = 50;
-		params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-		params.addRule(RelativeLayout.CENTER_VERTICAL);
-		next = new ImageView(context);
-		next.setImageResource(R.drawable.navigation_next_item);
-		next.setLayoutParams(params);
-		next.setId(3);
+
+		monthTextView = (TextView) calendarMonthLayout.findViewById(R.id.month_textview);
+		monthTextView.setText(context.getResources().getString(R.string.month_year,
+				cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()), cal.get(Calendar.YEAR)));
+
+		next = (ImageButton) calendarMonthLayout.findViewById(R.id.month_next_btn);
 		next.setOnClickListener(this);
-		
-		base.addView(next);
-		
-		addView(base);
-		
-		params = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-//		params.topMargin = 20;
-		params.bottomMargin = 20;
-		params.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-		params.addRule(RelativeLayout.BELOW, base.getId());
-		
-		calendar = new GridView(context);
-		calendar.setLayoutParams(params);
-		calendar.setVerticalSpacing(4);
-		calendar.setHorizontalSpacing(4);
-		calendar.setNumColumns(7);
-		calendar.setChoiceMode(GridView.CHOICE_MODE_SINGLE);
-		calendar.setDrawSelectorOnTop(true);
-		
-		mAdapter = new CalendarAdapter(context,cal);
-		calendar.setAdapter(mAdapter);
-		
-		addView(calendar);
+
+		calendarGridView = (GridView) calendarLayout.findViewById(R.id.calendar_gridView);
+		mAdapter = new CalendarAdapter(context, cal, Calendar.MONDAY);
+		calendarGridView.setAdapter(mAdapter);
+
+		addView(calendarLayout);
 	}
 
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		if(dayListener != null){
-			Day d = (Day) mAdapter.getItem(arg2);
-			if(d.getDay() != 0){
-				dayListener.onDayClicked(arg0, arg1, arg2, arg3,d);
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		if (dayListener != null) {
+			Day d = (Day) mAdapter.getItem(position);
+			if (d.getDay() != 0) {
+				dayListener.onDayClicked(parent, view, position, id, d);
 			}
 		}
 	}
-	
+
 	/**
-	 * 
 	 * @param listener
-	 * 
-	 * Set a listener for when you press on a day in the month
+	 *            Set a listener for when you press on a day in the month
 	 */
-	public void setOnDayClickListener(OnDayClickListener listener){
-		if(calendar != null){
+	public void setOnDayClickListener(OnDayClickListener listener) {
+		if (calendarGridView != null) {
 			dayListener = listener;
-			calendar.setOnItemClickListener(this);
+			calendarGridView.setOnItemClickListener(this);
 		}
 	}
-	
+
 	/**
-	 * 
 	 * @param contextListener
-	 * 
-	 * Set a context menu for days of the month
+	 *            Set a context menu for days of the month
 	 */
-	public void setOnCreateContextMenuListener(OnDayContextMenuListener contextListener){
-		if(calendar != null){
+	public void setOnCreateContextMenuListener(OnDayContextMenuListener contextListener) {
+		if (calendarGridView != null) {
 			this.contextListener = contextListener;
-			calendar.setOnCreateContextMenuListener(this);
+			calendarGridView.setOnCreateContextMenuListener(this);
 		}
 	}
 
 	@Override
-	public void onCreateContextMenu(ContextMenu menu, View v,ContextMenuInfo menuInfo) {
-		if(contextListener != null){
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		if (contextListener != null) {
 			contextListener.onDayContextMenu(menu, v, menuInfo);
 		}
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()){
-		case 1:
-			if(cal.get(Calendar.MONTH) == cal.getActualMinimum(Calendar.MONTH)) {				
-				cal.set((cal.get(Calendar.YEAR)-1),cal.getActualMaximum(Calendar.MONTH),1);
+		int vId = v.getId();
+
+		if (vId == R.id.month_prev_btn) {
+			if (cal.get(Calendar.MONTH) == cal.getActualMinimum(Calendar.MONTH)) {
+				cal.set((cal.get(Calendar.YEAR) - 1), cal.getActualMaximum(Calendar.MONTH), 1);
 			} else {
-				cal.set(Calendar.MONTH,cal.get(Calendar.MONTH)-1);
+				cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) - 1);
 			}
 			rebuildCalendar();
-			break;
-		case 3:
-			if(cal.get(Calendar.MONTH) == cal.getActualMaximum(Calendar.MONTH)) {				
-				cal.set((cal.get(Calendar.YEAR)+1),cal.getActualMinimum(Calendar.MONTH),1);
+		} else if (vId == R.id.month_next_btn) {
+			if (cal.get(Calendar.MONTH) == cal.getActualMaximum(Calendar.MONTH)) {
+				cal.set((cal.get(Calendar.YEAR) + 1), cal.getActualMinimum(Calendar.MONTH), 1);
 			} else {
-				cal.set(Calendar.MONTH,cal.get(Calendar.MONTH)+1);
+				cal.set(Calendar.MONTH, cal.get(Calendar.MONTH) + 1);
 			}
 			rebuildCalendar();
-			break;
-		default:
-			break;
 		}
 	}
-	
-	private void rebuildCalendar(){
-		if(month != null){
-			month.setText(cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()));
+
+	private void rebuildCalendar() {
+		if (monthTextView != null) {
+			monthTextView.setText(context.getResources().getString(R.string.month_year,
+					cal.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()), cal.get(Calendar.YEAR)));
 			refreshCalendar();
 		}
 	}
-	
+
 	/**
 	 * Refreshes the month
 	 */
-	public void refreshCalendar(){
+	public void refreshCalendar() {
 		mAdapter.refreshDays();
 		mAdapter.notifyDataSetChanged();
 	}
-	
+
 	/**
-	 * 
 	 * @param color
-	 * 
-	 * Sets the background color of the month bar
+	 *            Sets the background color of the month bar
 	 */
-	public void setMonthTextBackgroundColor(int color){
-		base.setBackgroundColor(color);
+	public void setMonthTextBackgroundColor(int color) {
+		calendarMonthLayout.setBackgroundColor(color);
 	}
-	
+
 	@SuppressLint("NewApi")
 	/**
-	 * 
 	 * @param drawable
-	 * 
-	 * Sets the background color of the month bar. Requires at least API level 16
+	 *            Sets the background color of the month bar. Requires at least API level 16
 	 */
-	public void setMonthTextBackgroundDrawable(Drawable drawable){
-		if(Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1){
-			base.setBackground(drawable);
+	public void setMonthTextBackgroundDrawable(Drawable drawable) {
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+			calendarMonthLayout.setBackground(drawable);
 		}
-		
 	}
-	
+
 	/**
-	 * 
 	 * @param resource
-	 * 
-	 * Sets the background color of the month bar
+	 *            Sets the background color of the month bar
 	 */
-	public void setMonehtTextBackgroundResource(int resource){
-		base.setBackgroundResource(resource);
+	public void setMonthTextBackgroundResource(int resource) {
+		calendarMonthLayout.setBackgroundResource(resource);
 	}
-	
+
 	/**
-	 * 
 	 * @param recource
-	 * 
-	 * change the image of the previous month button
+	 *            change the image of the previous month button
 	 */
-	public void setPreviousMonthButtonImageResource(int recource){
+	public void setPreviousMonthButtonImageResource(int recource) {
 		prev.setImageResource(recource);
 	}
-	
+
 	/**
-	 * 
 	 * @param bitmap
-	 * 
-	 * change the image of the previous month button
+	 *            change the image of the previous month button
 	 */
-	public void setPreviousMonthButtonImageBitmap(Bitmap bitmap){
+	public void setPreviousMonthButtonImageBitmap(Bitmap bitmap) {
 		prev.setImageBitmap(bitmap);
 	}
-	
+
 	/**
-	 * 
 	 * @param drawable
-	 * 
-	 * change the image of the previous month button
+	 *            change the image of the previous month button
 	 */
-	public void setPreviousMonthButtonImageDrawable(Drawable drawable){
+	public void setPreviousMonthButtonImageDrawable(Drawable drawable) {
 		prev.setImageDrawable(drawable);
 	}
-	
+
 	/**
-	 * 
 	 * @param recource
-	 * 
-	 * change the image of the next month button
+	 *            change the image of the next month button
 	 */
-	public void setNextMonthButtonImageResource(int recource){
+	public void setNextMonthButtonImageResource(int recource) {
 		next.setImageResource(recource);
 	}
-	
+
 	/**
-	 * 
 	 * @param bitmap
-	 * 
-	 * change the image of the next month button
+	 *            change the image of the next month button
 	 */
-	public void setNextMonthButtonImageBitmap(Bitmap bitmap){
+	public void setNextMonthButtonImageBitmap(Bitmap bitmap) {
 		next.setImageBitmap(bitmap);
 	}
-	
+
 	/**
-	 * 
 	 * @param drawable
-	 * 
-	 * change the image of the next month button
+	 *            change the image of the next month button
 	 */
-	public void setNextMonthButtonImageDrawable(Drawable drawable){
+	public void setNextMonthButtonImageDrawable(Drawable drawable) {
 		next.setImageDrawable(drawable);
 	}
 
